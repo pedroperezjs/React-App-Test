332ee61715ae33e11c2c419c75cac4dfe031b43c
PedroGj <pedrocsweb@gmail.com>
2025-10-15 11:48:42 -0300

    add custom hook useFecth

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useFetch } from './hooks/useFetch';
 import { Button } from './components';
 
+const url = 'https://api.escuelajs.co/api/v1/products';
+
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch('https://api.escuelajs.co/api/v1/products');
-      if (!response.ok) {
-        throw new Error('Error al obtener datos');
-      }
-      const dataResponse = await response.json();
-      setData(dataResponse);
-    } catch (err) {
-      setError(err as string);
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { data, error, loading } = useFetch(url);
 
   const handleClick = () => {
     console.log('My Button');
